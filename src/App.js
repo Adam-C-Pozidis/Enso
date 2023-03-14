dbd83756cb4ffadfe1dcac5d815b489446f9a2c1
Αδάμ Χρήστος Ποζίδης <apozidis@braincube.gr>
2023-03-14 12:41:32 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtLaLbkrEgpgvNWQD9q1WCOr5JqyynuZY",
+  authDomain: "enso-fitness-academy.firebaseapp.com",
+  projectId: "enso-fitness-academy",
+  storageBucket: "enso-fitness-academy.appspot.com",
+  messagingSenderId: "128523708087",
+  appId: "1:128523708087:web:73f3779ce729e1a8a0cf32",
+  measurementId: "G-4FCSW2TXL1",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
